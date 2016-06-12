5a231169e21e30b8b4dc58894bda00eed4436b4c
Bonome <bonome@Charming>
2016-06-12 12:00:18 +0200

    synchro github

--- gulpfile.js
+++ gulpfile.js
@@ -9,3 +9,17 @@ var gulp = require('gulp');
 gulp.task('default', function () {
     // place code for your default task here
 });
+
+// Gulp task for creating template cache
+gulp.task('templatecache', function() {
+    log('Creating an AngularJS $templateCache');
+
+    return gulp
+        .src(config.htmltemplates)
+        .pipe($.minifyHtml({empty: true}))
+        .pipe($.angularTemplatecache(
+            config.templateCache.file,
+            config.templateCache.options
+        ))
+        .pipe(gulp.dest(config.temp));
+});
